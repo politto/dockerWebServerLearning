ff151aa316b3dcce3eefb4a6184e311a45747a7d
Petchz87 <zpetza1547@gmail.com>
2023-10-24 23:35:55 +0700

    merge complete

--- src/static/script.js
+++ src/static/script.js
@@ -2,23 +2,6 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-function openPopup() {
-  console.log(popup);
-  popup.classList.add("open-popup");
-}
-
-function closePopup() {
-  popup.classList.remove("open-popup");
-}
-
-const locationHandler = require('../routes/pageRouter');
-
-console.log("hello world");
-
-// create a function that watches the hash and calls the urlLocationHandler
-window.addEventListener("hashchange", locationHandler);
-// call the urlLocationHandler to load the page
-locationHandler();
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
--- src/static/scriptSEC.js
+++ src/static/scriptSEC.js
@@ -2,6 +2,6 @@ document.getElementById("bt").addEventListener("click",function(){
     document.querySelector(".popupSEC").style.display = "flex";
 })
 
-document.querySelector(".close").addEventListener("click",function(){
-    document.querySelector(".popupSEC").style.display = "flex";
+document.querySelector("close").addEventListener("click",function(){
+    document.querySelector(".popupSEC").style.display = "none";
 })
