84e6692bb9f9df40e81435f59ec454d7cc9ef4c1
LunarticKnight <111190798+LunarticKnight@users.noreply.github.com>
2023-10-24 19:26:27 +0700

    adding hovering color popup setting

--- src/static/script.js
+++ src/static/script.js
@@ -1,9 +1,36 @@
-let popup = document.getElementById("popup");
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-function openPopup() {
-  popup.classList.add("open-popup");
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openSetting(modal)
+  })
+})
+
+ closeModalButtons.forEach(button => {
+   button.addEventListener('click', () => {
+     const set = button.closest('.modal')
+     closeSetting(set)
+  })
+ })
+
+function openSetting(set) {
+  if (set == null) return
+  set.classList.add('active')
+  overlay.classList.add('active')
 }
 
-function closePopup() {
-  popup.classList.remove("open-popup");
+function closeSetting(set) {
+  if (set == null) return
+  set.classList.remove('active')
+  overlay.classList.remove('active')
 }
+
+overlay.addEventListener('click', () => {
+  const over = document.querySelectorAll('.modal.active')
+ over.forEach(set => {
+    closeSetting(set)
+  })
+})
